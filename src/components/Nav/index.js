6a4381fe5bb7d8e3ad907aac69734b635b464996
Unisa Kamara <unisk434@gmail.com>
2022-05-18 18:28:52 +0000

    added  gallery/photolist

---
+++ src/components/Nav/index.js
@@ -0,0 +1,53 @@
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useEffect } from 'react';
+
+function Nav(props) {
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About me
+            </a>
+          </li>
+          <li className="mx-2">
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
